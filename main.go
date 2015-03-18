d793d156e1fd68701b76f394ff6bf31700bac867
francesco- <francesco.cislaghi@fyber.com>
2015-03-18 10:14:39 +0100

    no info

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ import (
 	"github.com/bigdatadev/goryman"
 	"github.com/golang/glog"
 	"github.com/google/cadvisor/client"
-	"github.com/google/cadvisor/info/v2"
+
 )
+
+//	"github.com/google/cadvisor/info/v2"
 
 var riemannAddress = flag.String("riemann_address", "localhost:5555", "specify the riemann server location")
 var cadvisorAddress = flag.String("cadvisor_address", "http://localhost:8080", "specify the cadvisor API server location")
